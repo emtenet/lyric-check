600de12a67c99c26722ac27c7b952e2ab6fd53ed
Michael Taylor <michael@emte.net.au>
2025-04-11 12:51:38 +1000

    Move testing cli aside

---
+++ src/bin/cli.rs
@@ -0,0 +1,85 @@
+use anyhow::Result;
+use askama::Template;
+
+use lyric_check::{
+    Diff,
+    DiffPage,
+};
+
+fn main() -> Result<()> {
+    let args: Vec<String> = std::env::args().skip(1).collect();
+    let args: Vec<&str> = args.iter().map(String::as_ref).collect();
+    match &args[..] {
+        ["html", music, script] => {
+            let xml = std::fs::read_to_string(music)?;
+            let txt = std::fs::read_to_string(script)?;
+            let page = DiffPage {
+                sections: lyric_check::diff::read(&xml, &txt)?,
+            };
+            let html = page.render()?;
+            println!("{html}");
+        }
+
+        ["diff", music, script] => {
+            let xml = std::fs::read_to_string(music)?;
+            let txt = std::fs::read_to_string(script)?;
+            let sections = lyric_check::diff::read(&xml, &txt)?;
+            for section in sections {
+                println!("{}", section.heading);
+                for line in section.lines {
+                    println!("{}", line.number);
+                    for diff in line.diffs {
+                        match diff {
+                            Diff::Same(text) =>
+                                println!("    {text}"),
+
+                            Diff::Music(text) =>
+                                println!("    ==> {text}"),
+
+                            Diff::Script(text) =>
+                                println!("    {text} <=="),
+
+                            Diff::Replace(replace) =>
+                                println!("    {} <=> {}", replace.script, replace.music),
+                        }
+                    }
+                }
+            }
+        }
+
+        ["music", file] => {
+            let xml = std::fs::read_to_string(file)?;
+            let part = lyric_check::music::read(&xml)?;
+            for phrase in &part.phrases {
+                for word in &phrase.words {
+                    print!("{} ", word.text);
+                    if !word.phrases.is_empty() {
+                        for phrase in &word.phrases {
+                            println!("<<");
+                            print!(" -->");
+                            for word in &phrase.words {
+                                print!("{} ", word.text);
+                            }
+                            print!(">>");
+                        }
+                    }
+                }
+                println!("");
+            }
+        }
+
+        ["script", file] => {
+            let txt = std::fs::read_to_string(file)?;
+            let words = lyric_check::script::read(&txt)?;
+            for word in words {
+                println!("{word:?}");
+            }
+        }
+
+        _ => {
+            println!("lyric-check {args:?}");
+        }
+    }
+    Ok(())
+}
+
